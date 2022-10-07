3a4a77c2d3412541ef05ff0a0756f020d9a427b1
Feast <1353216996@qq.com>
2022-10-08 00:45:31 +0800

    对导入配置进行调整，连接Electron事件
    正确将Electron和Html Document事件连接，必须走ipc

--- WeQ/electron/main.ts
+++ WeQ/electron/main.ts
@@ -1,19 +1,39 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow ,screen , webFrame ,ipcMain} from "electron";
 import * as path  from 'path';
 console.log(process.env.MODE);
 const loadUrl = process.env.MODE === "devlopment" ? "http://127.0.0.1:5173/" : path.join(__dirname, "../src/index.html");
-
-function createWindow() {
-  const win = new BrowserWindow({
+let win: BrowserWindow | null = null;
+const mode = 'development'
+function createWindow() { 
+  win = new BrowserWindow({
     width: 900,
     height: 600,
     title : "WeQ",
+    frame : false,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      // nodeIntegration: true,
+      preload: path.join(__dirname, "preload.js"), 
+      nodeIntegration: true,
     },
   });
+  const devInnerHeight = 1080.0 
+  const devDevicePixelRatio = 1.0// 开发时的devicepixelratio
+  const devScaleFactor = 1.3 // 开发时的ScaleFactor
+  const scaleFactor = screen.getPrimaryDisplay().scaleFactor
+  
   win.loadURL(loadUrl);
+  const zoomFactor = 
+  (window.innerHeight / devInnerHeight) * 
+  (window.devicePixelRatio / devDevicePixelRatio) * 
+  (devScaleFactor / scaleFactor)
+  webFrame.setZoomFactor(zoomFactor)
+
+  win.on('closed', () => {
+    win = null;
+  });
+
+  if (mode === 'development') {
+    win.webContents.openDevTools()
+  }
 }
 
 app.whenReady().then(() => {
@@ -31,4 +51,15 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
-
+// 实现自定义标题栏，最小化，最大化，关闭
+ipcMain.on("window-min", () => win?.minimize());
+ipcMain.on("window-max", () => {
+  if (win?.isMaximized()) {
+    win.unmaximize();
+  } else {
+    win?.maximize();
+  }
+});
+ipcMain.on("window-close", () => {
+  win?.destroy();
+});
--- WeQ/electron/preload.ts
+++ WeQ/electron/preload.ts
@@ -1,9 +1,15 @@
+import { ipcRenderer } from 'electron'
 window.addEventListener("DOMContentLoaded", () => {
     const replaceText = (selector, text) => {
       const element = document.getElementById(selector);
       if (element) element.innerText = text;
     };
-  
+    const miniButton = document.getElementById("MinimizeButton")
+    miniButton?.addEventListener("click",()=>ipcRenderer.send("window-min"));
+    
+    const closeButton = document.getElementById("CloseButton")
+    closeButton?.addEventListener("click",()=>ipcRenderer.send("window-close"));
+
     for (const type of ["chrome", "node", "electron"]) {
       replaceText(`${type}-version`, process.versions[type]);
     }
